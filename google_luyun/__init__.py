81aa538203e8067f586bdf9a6822029a8aff94e9
0096001623 <0096001623@zwxict.com>
2020-12-14 14:49:27 +0800

    tensorflow2.0视频教程 工程师google卢云

---
+++ google_luyun/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/4 22:18
+# @Author  : Denglw
+# @Email   : 892253193@qq.com
+# @File    : __init__.py.py
+# @desc:
---
+++ google_luyun/tf_keras_regression.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/4 22:20
+# @Author  : Denglw
+# @Email   : 892253193@qq.com
+# @File    : tf_keras_regression.py
+# @desc:
